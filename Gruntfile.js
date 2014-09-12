f1bd75c2d2c8c54e7cbd214aad6f87d7f6748169
Guannan He <guannan.he@sungard.com>
2014-09-11 17:47:38 -0700

    add grunt to project

---
+++ Gruntfile.js
@@ -0,0 +1,55 @@
+'ust strict';
+
+module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+
+	grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        jshint: {
+            // define the files to lint
+            files: ['gruntfile.js', 'src/public/javascript/*.js', 'test/**/*.js'],
+            // configure JSHint (documented at http://www.jshint.com/docs/)
+            options: {
+            // more options here if you want to override JSHint defaults
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true,
+                }
+            }
+        },
+
+        watch:{
+            express: {
+                files:  [ '<%= jshint.files %>' ],
+                tasks:  [ 'express:dev' ],
+                options: {
+                    spawn: false // for grunt-contrib-watch v0.5.0+, "nospawn: true" for lower versions. Without this option specified express won't be reloaded
+                }
+            }
+        },
+
+        open: {
+            server: {
+                url: 'http://localhost:3000'
+            }
+        },
+
+        express: {
+            options: {
+                debug: true
+            },
+            dev: {
+                options: {
+                    script: './bin/www',
+                }
+            },
+        }
+
+    });
+
+    grunt.registerTask('default', ['jshint', 'express:dev', 'watch']);
+
+};
